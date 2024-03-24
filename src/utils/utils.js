0dcdfcf9cfaca42a2d1bf83ba188ff352808fd70
venecoderr <147117046+venecoderr@users.noreply.github.com>
2024-03-24 09:33:46 -0400

    contact page done

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -14,3 +14,24 @@ export const fetchRepos = async () => {
 export function dashToSpace(inputString) {
     return inputString.split('-').join(' ');
 }
+
+export function validateEmail(email) {
+  const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  return re.test(String(email).toLowerCase());
+}
+
+export function classNames(...classes) {
+  return classes.filter(Boolean).join(' ')
+}
+
+export function highlighter(tabs, active) {
+  tabs.map((i) => {
+    if(i.href === active){
+      i.current = true
+    } else {
+      i.current = false
+    }
+  })
+
+  return tabs
+}
